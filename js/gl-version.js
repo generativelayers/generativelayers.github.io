22876ce16b0854c5923a826337c36fefd050de46
Dimitrios Kyriakidis <93831628+kyriakidisdimitrios@users.noreply.github.com>
2026-06-15 09:13:39 +0300

    Remove extra install sentence on getting started

--- js/gl-version.js
+++ js/gl-version.js
@@ -8,3 +8,9 @@
 window.GL_VERSION = '0.2.1';
 window.GOOGLE_CLIENT_ID = '814105936155-1p1s8p59lobkb2ugjbsrmjc9fvulsj6e.apps.googleusercontent.com';
 
+document.addEventListener('DOMContentLoaded', function () {
+  var installIntro = document.querySelector('#install p');
+  if (installIntro) {
+    installIntro.textContent = 'Generative Layers is available on Maven Central.';
+  }
+});
